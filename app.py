cfb30813a23e5184da2a01283fc3920224f6e915
KaiJie <65100809+ykj871030@users.noreply.github.com>
2023-12-21 00:09:07 +0800

    Name fix
    fix, fix and fix.

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    userID = event.source.user_id.text
+    userID = event.source.user_id
     userName = line_bot_api.get_profile(userID)
     sendContent=TextSendMessage(text='userID: ' + userID + '\n' + userName + ', 你說的是：\n' + msg + ' 嗎？')
     line_bot_api.reply_message(event.reply_token, sendContent)
