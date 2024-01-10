acd988705cdd7ccb2097eaf289999fc819a8e063
KaiJie <65100809+ykj871030@users.noreply.github.com>
2024-01-10 22:40:11 +0800

    New item: story begin.
    As title.

--- app.py
+++ app.py
@@ -10,25 +10,27 @@ from linebot.models import *
 
 from linebot.exceptions import LineBotApiError
 
+# ======這裡是呼叫的檔案內容=====
+# from message import *
+# from new import *
+# from Function import *
+# ======這裡是呼叫的檔案內容=====
 
-#======這裡是呼叫的檔案內容=====
-#from message import *
-#from new import *
-#from Function import *
-#======這裡是呼叫的檔案內容=====
-
-#======python的函數庫==========
+# ======python的函數庫==========
 import tempfile, os
 import datetime
 import time
-#======python的函數庫==========
+
+# ======python的函數庫==========
 
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 # Channel Access Token
-line_bot_api = LineBotApi('UJggamRStWntuURfiKYyCfwHKEI+7+28Mv7ZXYsiR+97FGzDMhpVXjibBYis3bFXTfTj4JCb5ufwFty7v+nI9FfwrXbMWrUWd6r0cYMGlzyyB/yXJ2szb9cui6hgih+D+vo5UHk37+ENPcNRIy9lzAdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi(
+    'UJggamRStWntuURfiKYyCfwHKEI+7+28Mv7ZXYsiR+97FGzDMhpVXjibBYis3bFXTfTj4JCb5ufwFty7v+nI9FfwrXbMWrUWd6r0cYMGlzyyB/yXJ2szb9cui6hgih+D+vo5UHk37+ENPcNRIy9lzAdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('b202c38b2526a4ca07fec24075461cf4')
+
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -53,21 +55,94 @@ def handle_message(event):
     userID = str(event.source.user_id)
     userName = line_bot_api.get_profile(userID).display_name
     if '開始遊戲' in msg:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text="(這裡會有四張圖片顯示，還有一個按鈕可以進入關卡)"))
+        story_carousel_template_message = TemplateSendMessage(alt_text='Carousel template',
+                                                              template=CarouselTemplate(columns=[
+                                                                  CarouselColumn(
+                                                                      thumbnail_image_url='https://example.com/國仁0-1.jpg',
+                                                                      title='國仁要去銀行',
+                                                                      text='國仁目前想要投資金融市場，所以前往銀行，詢問理財專員。',
+                                                                      actions=[
+                                                                          MessageAction(
+                                                                              label='國仁說：',
+                                                                              text='(0-1)國仁說：'
+                                                                          )
+                                                                      ]
+                                                                  ), CarouselColumn(
+                                                                      thumbnail_image_url='https://example.com/國仁0-2.jpg',
+                                                                      title='國仁抽號碼牌',
+                                                                      text='國仁到了銀行，遵守排隊原則，抽了張號碼牌。',
+                                                                      actions=[
+                                                                          MessageAction(
+                                                                              label='國仁說：',
+                                                                              text='(0-2)國仁說：'
+                                                                          )
+                                                                      ]
+                                                                  ), CarouselColumn(
+                                                                      thumbnail_image_url='https://example.com/國仁0-3.jpg',
+                                                                      title='國仁發現銀行的人都消失了',
+                                                                      text='抽完號碼牌的國仁發現，原本在銀行的人突然都不見了。',
+                                                                      actions=[
+                                                                          MessageAction(
+                                                                              label='國仁說：',
+                                                                              text='(0-3)國仁說：'
+                                                                          )
+                                                                      ]
+                                                                  ), CarouselColumn(
+                                                                      thumbnail_image_url='https://example.com/國仁0-4.jpg',
+                                                                      title='國仁看見有扇門是開著的',
+                                                                      text='但銀行內竟然有扇門是開著的，出自於好奇，國仁決定走進去一探究竟。',
+                                                                      actions=[
+                                                                          MessageAction(
+                                                                              label='國仁說：',
+                                                                              text='(0-4)國仁說：'
+                                                                          ), MessageAction(
+                                                                              label='進入房間：',
+                                                                              text='進入：開著的房間'
+                                                                          )
+                                                                      ]
+                                                                  )
+                                                              ]
+                                                              )
+                                                              )
+
+        line_bot_api.reply_message(event.reply_token, story_carousel_template_message)
+    elif '(0-1)國仁說：' in msg:
+        talkAbout = '最近剛賺了一筆，手上的閒置資金不知道要做什麼？你不理財，財不理你，閒著也是閒著，不如去銀行詢問理財專員吧！'
+        guoSay = TextSendMessage(text=talkAbout)
+        line_bot_api.reply_message(event.reply_token, guoSay)
+    elif '(0-2)國仁說：' in msg:
+        talkAbout = '找了理財專員才發現我還有很多手續要辦理，反正我也是第一次理財，就抽抽號碼牌，等櫃檯叫號吧！希望會被分配到妹子櫃檯。'
+        guoSay = TextSendMessage(text=talkAbout)
+        line_bot_api.reply_message(event.reply_token, guoSay)
+    elif '(0-3)國仁說：' in msg:
+        talkAbout = '今天銀行沒什麼人，看來也不需要排隊了，真幸運！不對，這是什麼情況？怎麼連櫃檯的人都沒有？難不成下班了嗎？我也才剛到銀行欸！。'
+        guoSay = TextSendMessage(text=talkAbout)
+        line_bot_api.reply_message(event.reply_token, guoSay)
+    elif '(0-4)國仁說：' in msg:
+        talkAbout = '咦？怎麼會有一扇門是開著的？這家銀行的控管誒太鬆散了吧！如果我偷偷跑進去應該也不會怎樣吧，反正銀行現在連半個人都沒有。'
+        guoSay = TextSendMessage(text=talkAbout)
+        line_bot_api.reply_message(event.reply_token, guoSay)
+    elif '進入：開著的房間' in msg:
+        line_bot_api.reply_message(event.reply_token,
+                                   TextSendMessage(text="(這邊使用ImageMap來製作關卡。)"))
     elif 'Open the door!' in msg:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="你知道門的密碼嗎？\n(輸入密碼時請打：門的密碼是：<密碼> )"))
+        line_bot_api.reply_message(event.reply_token,
+                                   TextSendMessage(text="你知道門的密碼嗎？\n(輸入密碼時請打：門的密碼是：<密碼> )"))
     elif '門的密碼是' in msg:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text="(這邊就會去資料庫判斷玩家的關卡去撈答案出來比對)"))
+        line_bot_api.reply_message(event.reply_token,
+                                   TextSendMessage(text="(這邊就會去資料庫判斷玩家的關卡去撈答案出來比對)"))
     elif 'inflation' in msg:
         try:
-            replyArray=[]
+            replyArray = []
             replyArray.append(TextSendMessage(text="inflation 通膨\n一種經濟現象，指一段時間內物價上升的幅度。"))
-            replyArray.append(AudioSendMessage(original_content_url='https://files.soundoftext.com/ad64dd70-67df-11ed-a44a-8501b7b1aefa.mp3', duration=1000))
+            replyArray.append(AudioSendMessage(
+                original_content_url='https://files.soundoftext.com/ad64dd70-67df-11ed-a44a-8501b7b1aefa.mp3',
+                duration=1000))
             line_bot_api.reply_message(event.reply_token, replyArray)
         except Exception as e:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=str(e)))
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str(e)))
     else:
-        sendContent=TextSendMessage(text=f'userID: {userID}\n{userName} 你說的是：\n{msg}嗎？')
+        sendContent = TextSendMessage(text=f'userID: {userID}\n{userName} 你說的是：\n{msg}嗎？')
         line_bot_api.reply_message(event.reply_token, sendContent)
     # if '最新合作廠商' in msg:
     #     message = imagemap_message()
@@ -91,20 +166,24 @@ def handle_message(event):
     #     message = TextSendMessage(text=msg)
     #     line_bot_api.reply_message(event.reply_token, message)
 
+
 @handler.add(PostbackEvent)
 def handle_message(event):
     print(event.postback.data)
 
-#加入好友的時候會跳出的訊息
+
+# 加入好友的時候會跳出的訊息
 @handler.add(FollowEvent)
 def welcome(event):
     userID = str(event.source.user_id)
     userName = line_bot_api.get_profile(userID).display_name
-    message = TextSendMessage(text=f'{userName} 您好，歡迎你加入Fin Game！\n你將會在Fin Game的世界學到\n有關金融報導的英文單字。\n若你已經準備好的話，請輸入「開始遊戲」吧！')
+    message = TextSendMessage(
+        text=f'{userName} 您好，歡迎你加入Fin Game！\n你將會在Fin Game的世界學到\n有關金融報導的英文單字。\n若你已經準備好的話，請輸入「開始遊戲」吧！')
     line_bot_api.reply_message(event.reply_token, message)
-        
-        
+
+
 import os
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
