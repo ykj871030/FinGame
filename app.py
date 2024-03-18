81ffce1e784e4702f02a97c4c9defe0d30168afc
KaiJie <65100809+ykj871030@users.noreply.github.com>
2024-03-19 00:28:19 +0800

    Good night.
    As title Zzz...

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def FinGameReview():
     <html lang="zh-Hant-TW">
         <head>
             <meta charset="UTF-8">
-            <meta name=”viewport” content=”width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0″>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
             <title>FinGame Review</title>
         </head>
         <body>
